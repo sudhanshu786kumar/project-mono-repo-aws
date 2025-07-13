d75214c35530d9abf782cc3fb3d98c1bd1f163b6
sudhanshu <sudhanshu786kumar@gmail.com>
2025-07-13 15:45:35 +0530

    inital commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MarketingApp from "./components/MarketinApp";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Container App</h1>
+      <MarketingApp />
+    </div>
+  );
+};
+
+export default App;
